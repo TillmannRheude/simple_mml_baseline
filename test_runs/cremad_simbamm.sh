39fc0eacaf855db4d11ca04e863a4b28200eef99
TillmannRheude <tillmann.rheude@gmx.de>
2025-12-15 15:00:43 +0100

    Cleaned

--- test_runs/cremad_simbamm.sh
+++ test_runs/cremad_simbamm.sh
@@ -5,20 +5,21 @@
 #SBATCH --gres=gpu:2
 #SBATCH --mem=200G
 #SBATCH --time 48:00:00
-#SBATCH --array=1-4
+#SBATCH --array=0-0
 
 split_nr=${SLURM_ARRAY_TASK_ID}
 
-# modelname.head_transformer.d_model=64 modelname.head_transformer.dim_feedforward=1024 modelname.head_transformer.dropout=0.1 modelname.head_transformer.nhead=4 modelname.head_transformer.num_layers=4 modelname.optimizer.lr=0.02946976043941851 modelname.optimizer.warmup_steps=1000 modelname.optimizer.weight_decay=0.01
+# modelname.head_transformer.d_model=512 modelname.head_transformer.dim_feedforward=256 modelname.head_transformer.dropout=0.2 modelname.head_transformer.nhead=8 modelname.head_transformer.num_layers=4 modelname.optimizer.lr=0.0033961339393781967 modelname.optimizer.warmup_steps=200 modelname.optimizer.weight_decay=0.001
 
-modelname_head_transformer_d_model=64
-modelname_head_transformer_dim_feedforward=1024
-modelname_head_transformer_dropout=0.1
-modelname_head_transformer_nhead=4
+modelname_head_transformer_d_model=512
+modelname_head_transformer_dim_feedforward=256
+modelname_head_transformer_dropout=0.2
+modelname_head_transformer_nhead=8
 modelname_head_transformer_num_layers=4
-modelname_optimizer_lr=0.02946976043941851
-modelname_optimizer_warmup_steps=1000
-modelname_optimizer_weight_decay=0.01
+modelname_optimizer_lr=0.0033961339393781967
+modelname_optimizer_warmup_steps=200
+modelname_optimizer_weight_decay=0.001
+
 batch_size=128
 
 python3 /sc-projects/sc-proj-ukb-cvd/projects/simple_mml_baseline_tr/main_incltest.py split_nr=${split_nr} \
@@ -29,12 +30,12 @@ python3 /sc-projects/sc-proj-ukb-cvd/projects/simple_mml_baseline_tr/main_inclte
         missing.missing_train=[0.0] \
         missing.missing_valid=[0.0] \
         missing.missing_test=[0.0] \
-        modelname.optimizer.lr=${modelname_optimizer_lr} \
-        modelname.optimizer.warmup_steps=${modelname_optimizer_warmup_steps} \
-        modelname.optimizer.weight_decay=${modelname_optimizer_weight_decay} \
         batch_size=${batch_size} \
         modelname.head_transformer.d_model=${modelname_head_transformer_d_model} \
         modelname.head_transformer.dim_feedforward=${modelname_head_transformer_dim_feedforward} \
         modelname.head_transformer.dropout=${modelname_head_transformer_dropout} \
         modelname.head_transformer.nhead=${modelname_head_transformer_nhead} \
-        modelname.head_transformer.num_layers=${modelname_head_transformer_num_layers}
+        modelname.head_transformer.num_layers=${modelname_head_transformer_num_layers} \
+        modelname.optimizer.lr=${modelname_optimizer_lr} \
+        modelname.optimizer.warmup_steps=${modelname_optimizer_warmup_steps} \
+        modelname.optimizer.weight_decay=${modelname_optimizer_weight_decay}
