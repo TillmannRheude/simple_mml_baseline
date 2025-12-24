b90921dce6f6f10d7207950501b63bf583f9840d
TillmannRheude <tillmann.rheude@gmx.de>
2025-12-24 10:28:48 +0100

    Preprint

--- codefiles/datasets/mosi_mosei.py
+++ codefiles/datasets/mosi_mosei.py
@@ -22,7 +22,8 @@ class MOSI_MOSEI(Dataset):
         self.variant = variant
 
         if dataset == "mosi":
-            self.datadir = f"/sc-projects/sc-proj-ukb-cvd/projects/data/MOSI/aligned_50_testsplits_split_{split_nr}.pkl"
+            #self.datadir = f"/sc-projects/sc-proj-ukb-cvd/projects/data/MOSI/aligned_50_testsplits_split_{split_nr}.pkl"
+            self.datadir = f"/sc-projects/sc-proj-ukb-cvd/projects/simple_mml_baseline_tr/archiv/file.pkl"
         elif dataset == "mosei":
             self.datadir = f"/sc-projects/sc-proj-ukb-cvd/projects/data/MOSEI/aligned_50_testsplits_split_{split_nr}.pkl"
 
